ec08a38f867a14d180bd169bef399ce3e4cb0190
benwata <91182154+benw4t4@users.noreply.github.com>
2022-03-20 06:38:27 +0800

    Add files via upload

---
+++ EXEC_SCRIPT_AMAKUDARI.js
@@ -0,0 +1,69 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+console.log("Was ki ra exec AMAKUDARI.");
+
+var mea=
+{
+    authority:"admin",
+    hymncode:"akari=kanna ansul ark:sphaela"
+}
+
+if (mea.authority === "admin")
+{
+    let emo="Ma num ra";
+    let act1="enter ciel en chs ar cyurio.";
+    let act2="exec sarla sor en gyusya omnis.";
+console.log(emo+" "+act1);
+console.log(emo+" "+act2);
+}
+
+var yor={locale:"ark:sphaela"}
+
+if(yor.locale === "ark:sphaela")
+{
+    let emoyor="Was yant ga";
+    let emomea="Was num ra";
+    let actyor="re deata sa yor.";
+    let actmea="won mahin mea.";
+
+console.log(emomea+" rre yorr "+actmea);
+console.log(emoyor+" rre yorr "+actyor);
+}
+
+
+    var sayor=Math.round(Math.random());
+    var kds=" 0x vvi.";
+    var kde="1x AAs ixi.";
+
+    if(sayor === 1)
+        {var yor="guilty";}
+    else
+        {var yor="innocent";}
+
+    if(yor === "guilty")
+    {
+        let emo="Was guwo ga";
+        let act1="viss cupla yor.";
+        let act2="na fogabe yor.";
+        let act3="zethpa manaf yor.";
+    console.log(emo+kds);
+    console.log(act1);
+    console.log(act2);
+    console.log(act3);
+    console.log(kde);
+    }
+    else
+    {
+        let emo="Ma num ra";
+        let act1="na cupla yor.";
+        let act2="dilete tes yor.";
+        let act3="khal ciel sor presia.";
+    console.log(emo+kds);
+    console.log(act1);
+    console.log(act2);
+    console.log(act3);
+    console.log(kde);
+    }
+
+console.log("Was yea ra knawa rre ciel exec cyurio mea.");
